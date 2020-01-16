ce06a139264efeb38032c489ee61b957d6748fd2
Micah Banschick <micha.ban@gmail.com>
2020-01-16 14:17:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,8 @@ def adding_matz
   a = {:yukihiro_matsumoto => {}}
   programmer_hash.merge(a)
   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}
-  b = programmer_hash[:yukihiro_matsumoto].each_key     
+  b = programmer_hash[:yukihiro_matsumoto].each_key{|key| key}
+  return b
 end
 
 def changing_alan
